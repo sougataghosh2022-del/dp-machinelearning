7b9149bf91aa57b958711a9314e21fe67c64e9b3
sougataghosh2022-del <sougataghosh2022@gmail.com>
2025-10-21 00:58:46 +0530

    Fix typo in scatter chart y-axis variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write('**Y**')
 Y = df.label
 Y
 with st.expander('Data visualization'):
-st.scatter_chart(data=df, x='bill length_mm', y='body_mass_g', color='species')
+st.scatter_chart(data=df, x='bill length_mm', y='body_mass_9', color='species')
 
 
 
