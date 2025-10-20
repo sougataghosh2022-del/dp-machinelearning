c50a055341c78efe4d9b4726af1bd8e49d64d8fb
sougataghosh2022-del <sougataghosh2022@gmail.com>
2025-10-20 13:26:04 +0530

    Add data expander to show raw data
    Added an expander section to display raw data from a CSV file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,10 @@ st.title('🤖Machine Learning App')
 
 st.write('This is app builds machine learning model!')
 
+with st.expander('Data');
+ st.write('**Raw data**')
+ df = pd.read_csv('https://raw.githubusercontent.com/sougataghosh2022-del/dp-machinelearning/refs/heads/master/Updated%20data_01_05_2025%20(1).csv')
+ df
+
 
 
