de99d6ff7439f6c7b880ad1b7b831ebd047605a0
sougataghosh2022-del <sougataghosh2022@gmail.com>
2025-10-20 13:08:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('🤖Machine Learning App')
 
 st.write('This is app builds machine learning model!')
-df = pd.read_csv('https://github.com/sougataghosh2022-del/dp-machinelearning/blob/master/Updated%20data_01_05_2025%20(1).csv')
+df = pd.read_csv('https://raw.githubusercontent.com/.csv')
 
