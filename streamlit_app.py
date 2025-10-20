8c040e9793ee75363272df58099fa307c3b409ef
sougataghosh2022-del <sougataghosh2022@gmail.com>
2025-10-20 12:33:03 +0530

    Add pandas import and load CSV data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖Machine Learning App')
 
 st.write('This is app builds machine learning model!')
+df=pd.read_csv('https://github.com/sougataghosh2022-del/dp-machinelearning/blob/master/Updated%20data_01_05_2025%20(1).csv')
