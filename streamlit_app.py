431a58332344cf86ab81ea304170c7e0763d2baa
sougataghosh2022-del <sougataghosh2022@gmail.com>
2025-10-21 00:51:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write('**Y**')
 Y = df.label
 Y
 with st.expander('Data visualization'):
- st.scatter_chart
+ st.scatter_chart(data=df, X='bill_length_mm', Y='body_mass_9', color='species')
 
 
 
